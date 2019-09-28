2fbaf7ca282a8bab57d5d0c06e005bb398d9c4f4
Barış Cirit <brscr@gmail.com>
2019-09-28 23:33:41 +0300

    first commit

---
+++ src/main/java/com/brscrt/springserversecured/SpringServerSecuredApplication.java
@@ -0,0 +1,13 @@
+package com.brscrt.springserversecured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServerSecuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServerSecuredApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/brscrt/springserversecured/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.brscrt.springserversecured.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.brscrt.springserversecured.pojo.Greeting;
+
+
+@Controller
+public class HelloWorldController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
